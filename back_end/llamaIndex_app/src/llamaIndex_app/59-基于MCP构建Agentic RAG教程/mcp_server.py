63ff7b0db83580e0a0cb667e0b81a11e6ad6415b
zhangzy625 <2661648928@qq.com>
2026-01-23 17:55:06 +0800

    d

---
+++ back_end/llamaIndex_app/src/llamaIndex_app/59-基于MCP构建Agentic RAG教程/mcp_server.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+from typing import  List
+from fastmcp import FastMCP
+
+from rag_app import FAQEngine, PYTHON_FAQ_TEXT
--- back_end/llamaIndex_app/src/llamaIndex_app/get_env.py
+++ back_end/llamaIndex_app/src/llamaIndex_app/get_env.py
@@ -4,7 +4,12 @@ from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[2]  # langchain_app/
 load_dotenv(ROOT / ".env")
+
 class Settings:
     METAPHOR_API_KEY = os.getenv("METAPHOR_API_KEY")
+    QDRANT_URL = "http://localhost:6333"
+    COLLECTION_NAME = "python_faq_collection"  # Using a new collection for the Python data
+    HOST = "127.0.0.1"
+    PORT = 8080
 
-setting = Settings();
+setting = Settings()
